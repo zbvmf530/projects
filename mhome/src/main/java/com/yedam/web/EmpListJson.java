5e114d2b7815c830cdbcd5f99eb58a6c7d6ed9b1
JSB <JSB@DESKTOP-TVVV04S>
2024-04-30 23:55:27 +0900

    숙제완료!

--- mhome/src/main/java/com/yedam/web/EmpListJson.java
+++ mhome/src/main/java/com/yedam/web/EmpListJson.java
@@ -1,7 +1,8 @@
 package com.yedam.web;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
--- mhome/src/main/webapp/js/array2.js
+++ mhome/src/main/webapp/js/array2.js
@@ -1,14 +1,14 @@
 let genderAry = [];
+let obj = {id: 1, first_name: '', last_name: '', email: '', gender: '', salary: ''};
+let props = ['id','first_name','email','salary'];
 
+let selects = document.querySelector('#genderList');
+selects.addEventListener('change', makeList); // 매개변수로 필터링된 배열 넘겨주면 될듯?
 
-let options = document.querySelector('#genderList');
-
-
-options.addEventListener('change',makeList()); // 매개변수로 필터링된 배열 넘겨주면 될듯?
 empList.forEach((item)=>{
 	if(genderAry.indexOf(item.gender) == -1){genderAry.push(item.gender);}
 });
-console.log(`성별 종류 : ${genderAry}`);
+//console.log(`성별 종류 : ${genderAry}`);
 
 genderAry.forEach(gender=>{
 	let opt = document.createElement('option');
@@ -17,12 +17,9 @@ genderAry.forEach(gender=>{
 	
 })
 
-//함수 (배열)
-function makeList(ary = []){
-	let obj = {id: 1, first_name: '', last_name: '', email: '', gender: '', salary: ''};
-	let props = ['id','first_name','email','salary'];
-	
-	ary.forEach(emp => {
+
+window.onload = function() {
+  empList.forEach(emp => {
 		let tr = document.createElement('tr');
 		props.forEach(prop => {
 			let td = document.createElement('td');
@@ -32,5 +29,24 @@ function makeList(ary = []){
 		document.querySelector('#show tbody').appendChild(tr);
 	});
 }
+//makeList(empList);
 
-makeList(empList);
+//함수 (배열)
+function makeList(){
+	document.querySelectorAll('#show tbody').item(0).innerHTML="";
+	let gender = selects.options[selects.selectedIndex].value;
+	
+	const result = empList.filter(function (value) {
+		return value.gender==gender; 
+	});
+
+	result.forEach(emp => {
+		let tr = document.createElement('tr');
+		props.forEach(prop => {
+			let td = document.createElement('td');
+			td.innerHTML = emp[prop];
+			tr.appendChild(td);
+		});
+		document.querySelector('#show tbody').appendChild(tr);
+	});
+}
